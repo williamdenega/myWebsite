2326a5ee0ab8d64951e9749477ec49797845ccf3
WilliamDenega <williamdenega@gmail.com>
2021-11-16 22:22:30 -0500

    Added tabs on the tob

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This will display informantion about my resume";
+//const h2 = document.createElement("h2");
+//h2.textContent = "This will display informantion about my resume";
 
-document.querySelector("body").appendChild(h2);
+//document.querySelector("body").appendChild(h2);
 
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContent = document.querySelectorAll('[data-tab-content')
+
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContent.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+        target.classList.add('active')
+    })
+})
